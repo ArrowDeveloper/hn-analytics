555810556c7a4754cdb63c0d3a1b2603d2483849
unknown <barayalsaurav4@gmail.com>
2026-04-25 21:47:44 +0545

    Test data for sqlalchemy model

--- src/hn/load_test_data.py
+++ src/hn/load_test_data.py
@@ -1,20 +1,14 @@
+from hn.model import User, Comment, Story, engine
+from sqlalchemy.orm import Session
 from datetime import datetime, timezone
-from hn.db import get_conn
 
-rows = [
-    ("alice", 1500, datetime(2015, 3, 1, tzinfo=timezone.utc), "about text"),
-    ("bob", 200, datetime(2020, 6, 15, tzinfo=timezone.utc), None),
-]
-stories = [(1,"alice", "testing", 34, datetime(2020, 5, 14, tzinfo=timezone.utc)),
-           (2, "bob", "helloworld", 37, datetime(2020, 3, 14, tzinfo=timezone.utc))]
+with Session(engine) as session:
+    testboi = User(name="testboi", karma_score=1000, created_at=datetime.now(timezone.utc))
+    testgurl = User(name="testgurl", karma_score=1300, created_at=datetime.now(timezone.utc))
+    story1 = Story(id=1, title="test", author=testboi, score=10, created_at=datetime.now(timezone.utc))
+    story2 = Story(id=2, title="test2", author=testgurl, score=20, created_at=datetime.now(timezone.utc))
+    comment1 = Comment(id=1, author=testboi, story = story1, created_at=datetime.now(timezone.utc))
+    comment2 = Comment(id=2, author=testgurl, story=story1, parent=comment1, created_at=datetime.now(timezone.utc))
+    session.add_all([testboi,testgurl,story1,comment1,comment2])
+    session.commit()
 
-comments = [(1, "alice", 1, None, datetime(2020, 3, 14, tzinfo=timezone.utc)),
-             (2, "bob", 1, 1, datetime(2020, 1, 14, tzinfo=timezone.utc)),
-             (3, "bob", 2, None, datetime(2020, 3, 15, tzinfo=timezone.utc)),
-]
-with get_conn() as conn:
-    with conn.cursor() as cur:
-        cur.executemany("INSERT INTO users (name, karma_score, created_at, about) VALUES (%s,%s,%s,%s)", rows)
-        cur.executemany("INSERT INTO stories (id, author_name, title, score, created_at) VALUES (%s,%s,%s,%s,%s)", stories)
-        cur.executemany("INSERT INTO comments (id, author_name, story_id, parent_comment, created_at) VALUES (%s,%s, %s, %s, %s)", comments)
-        conn.commit()
